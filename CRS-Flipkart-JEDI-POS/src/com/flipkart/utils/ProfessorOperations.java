26b536d9253d8724937cb47ef791f7980e99873a
sasirekhamurakonda <sasirekhamurakonda@gmail.com>
2022-01-29 22:20:09 +0530

    nothing to commit

--- CRS-Flipkart-JEDI-POS/src/com/flipkart/utils/ProfessorOperations.java
+++ CRS-Flipkart-JEDI-POS/src/com/flipkart/utils/ProfessorOperations.java
@@ -7,6 +7,7 @@ import java.sql.*;
 public class ProfessorOperations implements ProfessorUtilsInterface {
     public Professor validateCredentialsWithDB(int userId,String password){
         try{
+            //establishing connection
             Class.forName("com.mysql.jdbc.Driver");
             Connection con= DriverManager.getConnection(
                     "jdbc:mysql://localhost:3306/crsregistration","root","root");
